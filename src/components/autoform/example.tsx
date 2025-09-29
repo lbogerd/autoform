486ece1ab95022423ac852acf6d80bcc8f80279b
Lucas Bogerd <l.bogerd@protonmail.com>
2025-09-29 11:52:20 +0200

    fix: replace example with correct zod schema

--- src/components/autoform/example.tsx
+++ src/components/autoform/example.tsx
@@ -1,13 +1,11 @@
-import { asJsonSchema } from "../../../test/kitchen-sink-schema";
+import { KitchenSink } from "../../../test/kitchen-sink-schema";
 import { AutoForm } from "./auto-form";
 
 export const Example = () => {
-  const input = asJsonSchema;
-
   return (
     <section className="space-y-4 p-4 pb-12 max-w-xl m-auto">
       <h2 className="text-lg font-semibold">AutoForm</h2>
-      <AutoForm schema={input} />
+      <AutoForm schema={KitchenSink} />
     </section>
   );
 };
